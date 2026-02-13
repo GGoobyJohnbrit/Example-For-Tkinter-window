63a3314bedb457544d916d2732f5a8836ad77139
GGooby <victorstanciu69@gmail.com>
2026-02-13 17:17:17 +0200

    Enhance Tkinter app with message box and menu
    Added a message box feature and menu to the Tkinter app.

--- HelloWorld!/app.py
+++ HelloWorld!/app.py
@@ -1,7 +1,17 @@
 import tkinter as tk
-
+from tkinter import messagebox
 # Window
 root = tk.Tk()
+
+def msg_box():
+    messagebox.showinfo("Hello", "This is tkinter's message box example. pretty cool right?")
+
+my_menu = Menu(root)
+
+ex_menu = menu(my_menu, tearoff=0)
+my_menu.add_cascade(Label = "Example Menu Bar", menu=ex_menu)
+set_menu.add_command(label="Exit", command=root.quit)
+
 
 root.geometry("500x500")
 root.title("example")
@@ -15,4 +25,8 @@ textbox.pack(padx=10, pady=10)
 button = tk.Button(root, text="Click me!", font=('Arial', 18))
 button.pack(padx=10, pady=10)
 
+button = tk.Button(root, text="Msg box example", font=('Arial', 18), command=msg_box)
+button.pack(padx=10, pady=10)
+
 root.mainloop()
+
